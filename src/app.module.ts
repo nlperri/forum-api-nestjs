4c3bfb9edcb5cb7abf56bbb726d059eff6e10f15
natalia <lnataliaperri@gmail.com>
2023-11-02 15:37:50 -0300

    Add authentication configurations with JWT Token sign RS256

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { PrismaService } from "./prisma/prisma.service";
 import { CreateAccountController } from "./controllers/create-account.controller";
+import { envSchema } from "./env";
+import { AuthModule } from "./auth/auth.module";
+import { AuthenticateController } from "./controllers/authenticate-controller";
 
 @Module({
-  imports: [],
-  controllers: [CreateAccountController],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
